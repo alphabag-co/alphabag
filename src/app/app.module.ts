3fb7479d651457ec4a14b51e44472c497dedf760
Eugeniu Grigoras <eugeniugrigoras@Eugenius-MBP.station>
2018-02-24 03:02:07 +0100

    Million dollar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAo9V_2JL89nYfWK6-SkKk7gIsehhHwD2I',
+  authDomain: 'alphabag-1519203910493.firebaseapp.com',
+  databaseURL: 'https://alphabag-1519203910493.firebaseio.com',
+  projectId: 'alphabag-1519203910493',
+  storageBucket: 'alphabag-1519203910493.appspot.com',
+  messagingSenderId: '1012690676160'
+};
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
